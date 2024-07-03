17af62d8ec71aed4205dec8976c8758900e2b445
adyajudha <adityayudhapratama127@gmail.com>
2024-07-03 13:38:22 +0700

    jetstream auth

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,13 @@ Route::get('products', [ProductController::class, 'index']);
 Route::get('products/Edit/{id}/', [ProductController::class, 'edit']);
 Route::post('products/Store', [ProductController::class, 'store']);
 Route::get('products/Delete/{id}', [ProductController::class, 'destroy']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
